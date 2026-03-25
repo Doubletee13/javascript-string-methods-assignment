52130d326e33f6f4742594c522f80c6309dc933b
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2026-03-25 21:32:41 +0100

    setup: javascript string method task

---
+++ task.js
@@ -0,0 +1,63 @@
+/*
+INDIVIDUAL TASK
+==================
+1. Create an initials generator program
+
+- Ask for full name
+- Output initials
+
+Example:
+Andrew Solex → A.S
+*/
+let fullName = prompt("What is your fullname?")
+let splittedName = fullName.split(" ")
+console.log(`Your initials are: ${splittedName[0][0]}.${splittedName[1][0]}`)
+
+
+/*
+2. Create a word position finder
+
+- Ask for a sentence
+- Ask for a letter
+- Show the position of that letter
+*/
+
+let sentence = prompt("Enter a sentence")
+let letter = prompt(`Enter letter you wish to find it's position in your sentence`)
+let indexOfLetter = sentence.indexOf(letter)
+console.log(`letter '${letter}' is at position ${indexOfLetter + 1} in the sentence '${sentence}'`)
+
+/*
+3. Build a username generator
+
+- Ask for:
+-> First name
+-> Last name
+
+- Create a username like:
+andrewsolex_11
+
+(11 = total number of characters in first name and last name)
+*/
+
+let firstName = prompt("What is your first name?")
+let lastName = prompt("What is your last name?")
+let userName = firstName + lastName
+console.log(`Your username is ${userName}_${userName.length}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
